9e1340dc3296bfde54848d2e878b4571a3012b5b
Ollin Demian <ollin.langle@ciencias.unam.mx>
2019-08-27 21:59:50 -0400

    build a new base image with up to date software

--- migration-networks/common/ingest/src/asylum_seekers.py
+++ migration-networks/common/ingest/src/asylum_seekers.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import urllib.request
+import zipfile
+import requests
+import io
 import os
 import json
 import string
 import csv
 import pandas as pd
 
-path="/data/"
+path="/data"
 
 with urllib.request.urlopen("https://gist.githubusercontent.com/erdem/8c7d26765831d0f9a8c62f02782ae00d/raw/248037cd701af0a4957cce340dabb0fd04e38f4c/countries.json") as url:
     countries = json.loads(url.read().decode())
@@ -19,7 +22,7 @@ latlng = list(map(lambda x: x['latlng'],countries))
 lat = list(map(lambda x: x[0],latlng))
 lon = list(map(lambda x: x[1],latlng))
 
-cols = {'Country':names,'Code':codes,'lat':lat,'lon':lon}
+cols = {'Country':names,'lat':lat,'lon':lon}
 
 Countries = pd.DataFrame.from_dict(cols)
 
@@ -29,7 +32,7 @@ with urllib.request.urlopen("https://query.data.world/s/ab2po6isxjxe25awy7eougit
         if i == 4:
             columns = line.decode().replace("\"","").split(",")
             columns.pop()
-            del columns[1:4]
+            del columns[2:4]
             pop = pd.DataFrame(columns=columns)
         elif i > 4:
             row = line.decode().replace("\"","").split(",")
@@ -43,17 +46,53 @@ with urllib.request.urlopen("https://query.data.world/s/ab2po6isxjxe25awy7eougit
                 row[0] = "South Korea"
             rowcp = row.copy()
             row.pop()
-            del row[1:5]
-            if len(row) ==  58:
-                pop = pop.append(pd.Series(row,index=columns),ignore_index=True)
-            elif len(row) == 59:
+            if len(row) ==  63:
                 del row[1]
+                del row[2:5]
+                pop = pop.append(pd.Series(row,index=columns),ignore_index=True)
+            elif len(row) == 62:
+                del row[2:5]
                 pop = pop.append(pd.Series(row,index=columns),ignore_index=True)
         i += 1
 
-pop = pop.rename(index=str, columns={"Country Name":"Country"})
+r = requests.get("http://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv")
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall(path+"/raw")
+with open(path+"/raw/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_126271.csv", 'rb') as f:
+    i = 0
+    for line in f:
+        if i == 4:
+            columns = line.decode("utf-8").replace("\"","").split(",")
+            columns.pop()
+            del columns[2:4]
+            gdp = pd.DataFrame(columns=columns)
+        elif i > 4:
+            row = line.decode("utf-8").replace("\"","").split(",")
+            if "COG" in row[2]:
+                row[0] = "Congo"
+            if "COD" in row[2]:
+                row[0] = "DR Congo"
+            if "PRK" in row[2]:
+                row[0] = "North Korea"
+            if "KOR" in row[2]:
+                row[0] = "South Korea"
+            rowcp = row.copy()
+            row.pop()
+            if len(row) ==  64:
+                del row[1]
+                del row[2:4]
+                gdp = gdp.append(pd.Series(row,index=columns),ignore_index=True)
+            elif len(row) == 63:
+                del row[2:4]
+                gdp = gdp.append(pd.Series(row,index=columns),ignore_index=True)
+        i += 1
+
+pop = pop.rename(index=str, columns={"Country Name":"Country", "Country Code":"Code"})
+gdp = gdp.rename(index=str, columns={"Country Name":"Country", "Country Code":"Code"})
+
 for word in ["income","IBRD","only","total","blend","IDA","Not classified","members","World"]:
     pop=pop[~pop.Country.str.contains(word)]
+    gdp=gdp[~gdp.Country.str.contains(word)]
 
 drops = ["Arab World","Central Europe and the Baltics",
         "Channel Islands","Caribbean small states",
@@ -69,6 +108,7 @@ drops = ["Arab World","Central Europe and the Baltics",
         ]
 
 pop = pop[~pop["Country"].isin(drops)]
+gdp = gdp[~gdp["Country"].isin(drops)]
 
 fix_pop = {"Brunei Darussalam":"Brunei",
        "Cote d'Ivoire":"Ivory Coast",
@@ -89,14 +129,22 @@ fix_pop = {"Brunei Darussalam":"Brunei",
        "Curacao":"Curaçao",
        "Hong Kong SAR":"Hong Kong",
        "Macao SAR":"Macau",
+       "North Macedonia":"Macedonia",
+       "Swaziland":"Eswatini",
        }
 
 pop = pop.replace({"Country": fix_pop})
+gdp = gdp.replace({"Country": fix_pop})
 
-Countries = Countries.set_index("Country").join(pop.set_index("Country")).reset_index()
-list(Countries)
+gdp = gdp.replace({"Code": {"XKX":"KSV"}})
 
-Countries.to_csv(path+"clean/countries.csv",index=False,header=False,sep="|")
+Countries_pop = Countries.set_index("Country").join(pop.set_index("Country")).reset_index()
+Countries_pop = Countries_pop.drop("2016",axis=1)
+
+Countries_gdp = Countries.set_index("Country").join(gdp.set_index("Country")).reset_index()
+
+Countries_pop.to_csv(path+"/clean/countries_pop.csv",index=False,header=False,sep="|")
+Countries_gdp.to_csv(path+"/clean/countries_gdp.csv",index=False,header=False,sep="|")
 
 fix = {"Dem. Rep. of the Congo":"DR Congo",
        "Congo":"Republic of the Congo",
@@ -146,12 +194,12 @@ data = ["asylum_seekers_monthly.csv",
 
 def get_file(files):
     url = "http://popstats.unhcr.org/en/"+files
-    response = urllib.request.urlretrieve(url,path+"raw/"+files)
+    response = urllib.request.urlretrieve(url,path+"/raw/"+files)
 
 def remove_header(files):
     n = 0
-    with open(path+"raw/"+files,'rb') as fin,\
-            open(path+"raw/_"+files, 'wb') as fout:
+    with open(path+"/raw/"+files,'rb') as fin,\
+            open(path+"/raw/_"+files, 'wb') as fout:
         for line in fin:
             n += 1
             line = line.decode('Latin-1').encode('utf-8')
@@ -159,13 +207,13 @@ def remove_header(files):
                 fout.write(line)
 
 def clean_countries(files):
-    df = pd.read_csv(path+"raw/_"+files,low_memory=False)
+    df = pd.read_csv(path+"/raw/_"+files,low_memory=False)
     df = df.replace({"Country / territory of asylum/residence": fix})
     if 'Origin' in df.columns:
         df = df.replace({"Origin": fix})
         df = df[df['Origin'].isin(names)]
     df = df[df['Country / territory of asylum/residence'].isin(names)]
-    df.to_csv(path+"clean/"+files,index=False,header=False,sep="|")
+    df.to_csv(path+"/clean/"+files,index=False,header=False,sep="|")
 
 
 if __name__ == '__main__':
